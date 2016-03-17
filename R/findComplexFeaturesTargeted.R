601f14d4b3db1765afe822059665e513b3211a76
hafenr <robin.hafen@gmail.com>
2016-03-17 17:02:38 +0100

    Make computation more memory efficient and add option to parallelize

--- R/findComplexFeaturesTargeted.R
+++ R/findComplexFeaturesTargeted.R
@@ -17,6 +17,8 @@
 #'        algorithm. Optional.
 #' @param window.size The window size to use in the sliding window algorithm.
 #'        Optional.
+#' @param parallelized If the computation should be done in parallel.
+#'        Optional.
 #' @param n.cores The number of cores to use for parallel processing. Optional.
 #' @return A list containing the individual results of the sliding window
 #'         algorithm, one for each complex contained in
@@ -29,67 +31,56 @@ findComplexFeaturesTargeted <- function(protein.traces,
                                         complex.protein.assoc,
                                         corr.cutoff=0.99,
                                         window.size=15,
+                                        parallelized=FALSE,
                                         n.cores=parallel::detectCores()) {
-    # Merge the association information into the trace DT to produce
-    # a large DT where each row corresponds to the trace of a complex subunit.
     setkey(protein.traces, protein_id)
-    setkey(complex.protein.assoc, protein_id)
-    protein.traces.c <- merge(protein.traces,
-                              complex.protein.assoc[, list(protein_id, complex_id)],
-                              by='protein_id',
-                              allow.cartesian=TRUE)
-    # Generate decoys
-    set.seed(42)
-    protein.traces.decoy <- data.table::copy(protein.traces.c)
-    protein.traces.decoy[, complex_id := sample(complex_id)]
-    protein.traces.decoy[, complex_id := paste0('DECOY_', complex_id)]
-
-    protein.traces.decoy[, is_decoy := TRUE]
-    protein.traces.c[, is_decoy := FALSE]
-
-    ## Concatenate target and decoy complexes
-    protein.traces.all <- rbind(protein.traces.decoy, protein.traces.c)
-    # Throw out all complexes with less than 2 traces
-    protein.traces.all[, n_subunits_observed := length(protein_id), by='complex_id']
-    protein.traces.all <- protein.traces.all[n_subunits_observed >= 2]
-    protein.traces.all[, n_subunits_observed := NULL]
+    setkey(complex.protein.assoc, complex_id)
 
     ## All complexes used for input
-    input.complexes.sw <- unique(protein.traces.all$complex_id)
+    input.complexes <- unique(complex.protein.assoc$complex_id)
 
-    cl <- snow::makeCluster(n.cores)
-    doSNOW::registerDoSNOW(cl)
-
-    setkey(protein.traces.all, complex_id)
-
-    sw.results <- foreach(i=seq_along(input.complexes.sw)) %do% {
-        complex.id <- input.complexes.sw[i]
+    runSlidingWindow <- function(i) {
+        complex.id <- input.complexes[i]
         cat('********************************************************************************\n')
-        cat(sprintf('CHECKING RUN:  %d / %d', i, length(input.complexes.sw)), '\n')
+        cat(sprintf('CHECKING RUN:  %d / %d', i, length(input.complexes)), '\n')
         cat('********************************************************************************\n')
-
         # Extract the protein traces belonging to the current complex
-        traces.subs <- protein.traces.all[complex_id == complex.id]
-        is.decoy.complex <- traces.subs$is_decoy[1]
+        complex.subunits <- complex.protein.assoc[complex_id == complex.id,
+                                                  protein_id]
+        traces.subs <- protein.traces[protein_id %in% complex.subunits]
         protein.names <- traces.subs$protein_id
         # Convert traces to a matrix
-        traces.mat <- as.matrix(subset(traces.subs,
-                                       select=-c(complex_id, protein_id, is_decoy)))
-
+        traces.mat <- as.matrix(subset(traces.subs, select=-protein_id))
         # Run the algorithm
         try({
-            findComplexFeatures(traces.mat, protein.names,
-                                corr.cutoff=corr.cutoff,
-                                window.size=window.size)
+            if (nrow(traces.mat) >= 2) {
+                findComplexFeatures(traces.mat, protein.names,
+                                    corr.cutoff=corr.cutoff,
+                                    window.size=window.size)
+            } else {
+                list()
+            }
         })
     }
 
-    res <- list(sw_results=sw.results,
-                input_complexes=input.complexes.sw,
+    if (parallelized) {
+        cl <- snow::makeCluster(n.cores)
+        doSNOW::registerDoSNOW(cl)
+
+        sw.results <- foreach(i=seq_along(input.complexes)) %dopar% {
+            runSlidingWindow(i);
+        }
+    } else {
+        sw.results <- foreach(i=seq_along(input.complexes)) %do% {
+            runSlidingWindow(i);
+        }
+    }
+
+    res <- list(sw.results=sw.results,
+                input.complexes=input.complexes,
                 corr.cutoff=corr.cutoff,
                 window.size=window.size,
-                protein.complex.assoc=subset(protein.traces.all,
-                                             select=c(protein_id, complex_id)))
+                complex.protein.assoc=complex.protein.assoc)
 
     class(res) <- 'targetedComplexFeaturesSW'
 
