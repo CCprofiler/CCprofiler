e55d9561034ddba792bf2c7d337ead312e676a0f
hafenr <robin.hafen@gmail.com>
2016-03-17 13:07:35 +0100

    Add sample data for protein-complex associations

--- R/data.R
+++ R/data.R
@@ -1,2 +1,14 @@
 #' Some protein traces of e4 that were filtered. TESTING DATA.
 'protein.traces'
+
+#' A sample data.table that holds information on CORUM complex membership.
+#'
+#' Each row in this data.table corresponds to an association between a CORUM
+#' complex and a protein subunit.
+#' The structure of this data.table is as follows:
+#' \itemize{
+#'  \item \code{complex_id} The numeric id of a CORUM complex. 
+#'  \item \code{complex_name} A character string of the complex' name.
+#'  \item \code{protein_id} The Uniprot identifier of the protein.
+#' }
+'corum.complex.protein.assoc'
