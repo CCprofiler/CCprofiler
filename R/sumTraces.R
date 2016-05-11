78b062e1483f5f7981f1d9635f6955d92a33e8a1
hafenr <robin.hafen@gmail.com>
2016-05-11 11:13:24 +0200

    Comment out old code

--- R/sumTraces.R
+++ R/sumTraces.R
@@ -1,23 +1,24 @@
 #' Produce analyte traces by summing traces of fragments. 
 #' @param trace.obj An object of class \code{FragmentTraces}.
-#' @return An object of type \code{FragmentTraces} belonging to a higher
-#'         level of the compositional hierarchy.
+#' @param by The annotation column that should be used when grouping fragments
+#'        for summation.
+#' @return An object of type \code{FragmentTraces} with summed traces.
 #' @export
-sumTraces <- function(trace.obj) {
-    traces.long <- toLongFormat(trace.obj$traces)
-    analyte.map <- trace.obj$analyte_map[, list(fragment_id, analyte_id)]
-    traces.long.with.analyte <-
-        merge(traces.long, analyte.map, by='fragment_id',
-              allow.cartesian=TRUE)
-    analyte.traces.long <-
-        traces.long.with.analyte[, list(intensity=sum(intensity)),
-                                 by=list(analyte_id, fraction)]
-    setnames(analyte.traces.long, 'analyte_id', 'fragment_id')
-    analyte.traces.wide <- toWideFormat(analyte.traces.long)
-    trace.obj.copy <- trace.obj
+sumTraces <- function(trace.obj, by='protein_id') {
+    # traces.long <- toLongFormat(trace.obj$traces)
+    # analyte.map <- trace.obj$analyte_map[, list(fragment_id, analyte_id)]
+    # traces.long.with.analyte <-
+    #     merge(traces.long, analyte.map, by='fragment_id',
+    #           allow.cartesian=TRUE)
+    # analyte.traces.long <-
+    #     traces.long.with.analyte[, list(intensity=sum(intensity)),
+    #                              by=list(analyte_id, fraction)]
+    # setnames(analyte.traces.long, 'analyte_id', 'fragment_id')
+    # analyte.traces.wide <- toWideFormat(analyte.traces.long)
+    # trace.obj.copy <- trace.obj
 
-    list(traces=analyte.traces.wide,
-         fragment_type=trace.obj$analyte_type,
-         analyte_type=NULL,
-         analyte_map=NULL)
+    # list(traces=analyte.traces.wide,
+    #      fragment_type=trace.obj$analyte_type,
+    #      analyte_type=NULL,
+    #      analyte_map=NULL)
 }
