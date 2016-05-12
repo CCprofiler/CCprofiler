2314fc386a0836b2f4bd3b293f8d5f97af6715b4
hafenr <robin.hafen@gmail.com>
2016-05-12 16:41:43 +0200

    Adjust coding style

--- R/filterByPeptideCorrelation.R
+++ R/filterByPeptideCorrelation.R
@@ -4,46 +4,51 @@
 #' Import peptide profiles from an OpenSWATH experiment.
 #' @name filterByPeptideCorrelation
 #' @import data.table
-#' @description filters peptide elution profiles based on sibling peptide correlation. 
-#' @param Traces Object of class Traces containing the quantitative MS data table.
-#' @param min_pairwise_corr Minimum pairwise pearson correlation necessary to retain the data for a protein with 2 peptides.
-#' @param average_corr_cutoff For proteins with >= 3 peptides, retain all in the data whose average correlation with all siblings is
-#'       how good, defaults to ">= median" but can be replaces with a numeric cutoff (e.g. 0.6).
-#'        
-#' @return An object of class Traces (peptide level) containing table.wide, table.long and id} that can be processed with the herein contained functions.
-#'         
+#' @description filters peptide elution profiles based on sibling peptide
+#'     correlation. 
+#' @param traces Object of class traces containing the quantitative MS data
+#'     table.
+#' @param min.pairwise.corr Minimum pairwise pearson correlation necessary
+#'     to retain the data for a protein with 2 peptides.
+#' @param average.corr.cutoff For proteins with >= 3 peptides, retain all
+#'     in the data whose average correlation with all siblings is how good,
+#'     defaults to '>= median' but can be replaces with a numeric cutoff
+#'     (e.g. 0.6).
+#' @return An object of class traces (peptide level) containing table.wide,
+#'     table.long and id that can be processed with the herein contained
+#'     functions.
 #' @export
-
-filterByPeptideCorrelation <- function(Traces, min_pairwise_corr = 0.8, average_corr_cutoff = ">= median"){
-  
-  data <- Traces$traces.wide
-  quantdata <- as.matrix(data[,3:ncol(data), with = FALSE])
-  labels <- data[,1:2]
+filterByPeptideCorrelation <- function(traces,
+                                       min.pairwise.corr=0.8,
+                                       average.corr.cutoff='>= median') {
+  data <- traces$traces.wide
+  quantdata <- as.matrix(data[, 3:ncol(data), with=FALSE])
+  labels <- data[, 1:2]
   rownames(quantdata) <- data$peptide_id
   proteins <- unique(data$protein_id)
   nproteins <- length(proteins)
-  passed <- logical(length = nrow(data))
-  for (i in 1:nproteins){
-    message(paste("PROCESSED", i, "of", nproteins, "proteins"))
+  passed <- logical(length=nrow(data))
+  for (i in 1:nproteins) {
+    message(paste('PROCESSED', i, 'of', nproteins, 'proteins'))
     indexpos <- proteins[i] == data$protein_id
-    df <- quantdata[indexpos,]
+    df <- quantdata[indexpos, ]
     df_cor <- cor(t(df))
     if (isTRUE(nrow(df) == 2)){ # If there's only 2 peptides, request minpaircorr
       paircorr <- mean(rowMeans(df_cor))
-      if (isTRUE(paircorr >= min_pairwise_corr)) {
+      if (isTRUE(paircorr >= min.pairwise.corr)) {
         passed[indexpos] <- TRUE
       } else{
         passed[indexpos] <- FALSE
       }
-    } else if (isTRUE(nrow(df) >= 3)) { 
+    } else if (isTRUE(nrow(df) >= 3)) {
       # If there's 3 or more peps, keep those correlated
       # equal or better than median
       avgcorr <- rowMeans(df_cor)
-      if (average_corr_cutoff == ">= median"){
-        survivors <- names(avgcorr[avgcorr >= median(avgcorr) ] )
+      if (average.corr.cutoff == '>= median') {
+        survivors <- names(avgcorr[avgcorr >= median(avgcorr)])
       }
-      if (class(average_corr_cutoff) == "numeric"){
-        survivors <- names(avgcorr[avgcorr >= average_corr_cutoff ] )
+      if (class(average.corr.cutoff) == 'numeric') {
+        survivors <- names(avgcorr[avgcorr >= average.corr.cutoff])
       }
       indexpos <- data$peptide_id %in% survivors
       passed[indexpos] <- TRUE
@@ -51,7 +56,7 @@ filterByPeptideCorrelation <- function(Traces, min_pairwise_corr = 0.8, average_
       passed[indexpos] <- FALSE
     }
   }
-  Result <- list(traces.wide = data[passed,], ids = Traces$ids[passed,]) 
-  class(Result) <- "Traces"
-  Result #return the filtered Traces data
+  result <- list(traces.wide=data[passed,], ids=traces$ids[passed,])
+  class(result) <- 'traces'
+  result #return the filtered traces data
 }
--- R/filterConsecutiveIdStretches.R
+++ R/filterConsecutiveIdStretches.R
@@ -1,77 +1,75 @@
 # For some pretty weird reason
 .datatable.aware=TRUE
 
-#' filterConsecutiveIdStretches filters protein profiling traces based on minimal length of consecutive ID stretches.
+#' filterConsecutiveIdStretches filters protein profiling traces based on
+#' minimal length of consecutive ID stretches.
 #'  
 #' @import data.table
-#' @param Traces object of class Traces containing the elution profiles that are to be filtered.
-#' @param min_stretch_len The minimal length a stretch of contiguous identifications
-#'        has to have in order not to be removed.
-#' @param remove_empty Logical whether the entries with rowSum == 0 after filtering are removed, defaults to TRUE. 
-#' @return An object of class Traces containing the filtered
-#'         chromatograms. 
-#'
+#' @param traces object of class traces containing the elution profiles that
+#'     are to be filtered.
+#' @param min_stretch_len The minimal length a stretch of contiguous
+#'     identifications has to have in order not to be removed.
+#' @param remove_empty Logical whether the entries with rowSum == 0 after
+#'     filtering are removed, defaults to TRUE. 
+#' @return An object of class traces containing the filtered
+#'     chromatograms. 
 #' @export
-#' 
-
-filterConsecutiveIdStretches<-function(Traces, min_stretch_length = 3, remove_empty = TRUE){
-  
-  #get Traces from container
-  
-  peptide.traces <- as.data.frame(Traces$peptide.traces)
-  
-  #Define n as the number of columns
-  labels <- peptide.traces[,1:2]
-  data <- peptide.traces[,3:ncol(peptide.traces)]
-  
-  #add 0-column
-  data <- cbind(data, dummy = rep(0, nrow(data)))
-  
-  #Filter
+filterConsecutiveIdStretches<-function(traces,
+                                       min_stretch_length=3,
+                                       remove_empty=TRUE) {
+  # Get traces from container
+  peptide.traces <- as.data.frame(traces$peptide.traces)
+  # Define n as the number of columns
+  labels <- peptide.traces[, 1:2]
+  data <- peptide.traces[, 3:ncol(peptide.traces)]
+  # Add 0-column
+  data <- cbind(data, dummy=rep(0, nrow(data)))
+  # Filter
   n <- ncol(data)
   # Set count variable to one
   tmp <- 1
-  # going through all rows, for all do:
+  # Going through all rows, for all do:
   for (x in 1:nrow(data)) {
-    message(paste("processed", x, "of", nrow(data), "rows"))
+    message(paste('processed', x, 'of', nrow(data), 'rows'))
     tmp <- 1
-    # go through values in all cols, from left to right and ask the following:
-    for (i in 1:n){
-      #is value in row x, col i = 0?
-      if (data[x,i]==0) {
-        # if the value is = o, tmp stays = 1
+    # Go through values in all cols, from left to right and ask the following:
+    for (i in 1:n) {
+      # Is value in row x, col i = 0?
+      if (data[x,i] == 0) {
+        # If the value is = o, tmp stays = 1
         tmp<-1
       }
-      #if value not = 0, then:
+      # If value not = 0, then:
       else {
-        #look at next value at row x, col i+1, is it 0? If yes, then:
-        if (data[x, i+1]==0) {
-          # do nothing if the count variable is >3, i.e. there's 4 or more values in a stretch
-          if (tmp>(min_stretch_length-1)) {}
+        # Look at next value at row x, col i+1, is it 0? If yes, then:
+        if (data[x, i+1] == 0) {
+          # Do nothing if the count variable is >3, i.e. there's 4 or more values in a stretch
+          if (tmp > (min_stretch_length-1)) {}
           else {
-            # replace values if count variable is 3 or smaller at x, i, where the next value is 0. Replace with 0
+            # Replace values if count variable is 3 or smaller at x, i, where the next value is 0. Replace with 0
             for (j in 0:tmp-1) {
               data[x, i-j] <- 0
             }
             tmp <- 1
           }
         }
-        # if next value is not = 0, then count tmp+1
+        # If next value is not = 0, then count tmp+1
         else {
-          tmp = tmp+1
+          tmp <- tmp+1
         }
       }
     }
   }
-  #remove dummy column
+
+  # Remove dummy column
   data <- subset(data, select =-dummy)
-  #write data to traces object
+  # Write data to traces object
   peptide.traces.filtered <- cbind(labels, data)
   if (remove_empty) {
-    peptide.traces.filtered <- peptide.traces.filtered[rowSums(data) != 0,]
+    peptide.traces.filtered <- peptide.traces.filtered[rowSums(data) != 0, ]
   }
-  Traces$peptide.traces <- as.data.table(peptide.traces.filtered)
-  return(Traces)
+  traces$peptide.traces <- as.data.table(peptide.traces.filtered)
+  return(traces)
 }
 
 # tests
