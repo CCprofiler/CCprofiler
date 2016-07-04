1f6162d2ebda5a7358ab02f6ce8bf5a06f10f3de
Heusel  Moritz <heuselm@ethz.ch>
2016-07-04 16:55:48 +0200

    change filterByPepCorr to new container format

--- R/filterByPeptideCorrelation.R
+++ R/filterByPeptideCorrelation.R
@@ -18,23 +18,21 @@
 #'     table.long and id that can be processed with the herein contained
 #'     functions.
 #' @export
-filterByPeptideCorrelation <- function(traces,
+filterByPeptideCorrelation <- function(Traces,
                                        min.pairwise.corr=0.8,
                                        average.corr.cutoff='>= median') {
-  data <- traces$traces.wide
-  quantdata <- as.matrix(data[, 3:ncol(data), with=FALSE])
-  labels <- data[, 1:2]
-  rownames(quantdata) <- data$peptide_id
-  proteins <- unique(data$protein_id)
+  quantdata <- getIntensityMatrix(Traces)
+  labels <- merge(Traces$traces$id, Traces$traces_annotation, by = "id")
+  proteins <- unique(labels$protein_id)
   nproteins <- length(proteins)
   passed <- logical(length=nrow(data))
   for (i in 1:nproteins) {
     message(paste('PROCESSED', i, 'of', nproteins, 'proteins'))
-    indexpos <- proteins[i] == data$protein_id
+    indexpos <- proteins[i] == labels$protein_id
     df <- quantdata[indexpos, ]
     df_cor <- cor(t(df))
     if (isTRUE(nrow(df) == 2)){ # If there's only 2 peptides, request minpaircorr
-      paircorr <- mean(rowMeans(df_cor))
+      paircorr <- mean(df_cor[1,2])
       if (isTRUE(paircorr >= min.pairwise.corr)) {
         passed[indexpos] <- TRUE
       } else{
@@ -42,8 +40,8 @@ filterByPeptideCorrelation <- function(traces,
       }
     } else if (isTRUE(nrow(df) >= 3)) {
       # If there's 3 or more peps, keep those correlated
-      # equal or better than median
-      avgcorr <- rowMeans(df_cor)
+      # equal or better than cutoff
+      avgcorr <- rowMeans(df_cor) - 1/nrow(df)
       if (average.corr.cutoff == '>= median') {
         survivors <- names(avgcorr[avgcorr >= median(avgcorr)])
       }
@@ -56,7 +54,11 @@ filterByPeptideCorrelation <- function(traces,
       passed[indexpos] <- FALSE
     }
   }
-  result <- list(traces.wide=data[passed,], ids=traces$ids[passed,])
-  class(result) <- 'traces'
-  result #return the filtered traces data
+  traces_filtered <- Traces$traces[passed,]
+  traces_annotation_filtered <- Traces$traces_annotation[passed,]
+  
+  result <- Traces
+  result$traces <- traces_filtered
+  result$traces_annotation <- traces_annotation_filtered
+  return(result) #return the filtered Traces data
 }
