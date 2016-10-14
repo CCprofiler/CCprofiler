673251f0f8f98cf3b8844f36fd9923020391e04e
isabell.bludau <isabell.bludau@gmail.com>
2016-10-14 11:11:08 +0200

    add function to namespace

--- R/annotateTraces.R
+++ R/annotateTraces.R
@@ -1,3 +1,10 @@
+#' Import peptide profiles from an OpenSWATH experiment.
+#' @import data.table
+#' @param traces.obj An object of type \code{traces.obj}.
+#' @param annotation.table path to tab-separated .txt file for annotation of the traces
+#' @return traces.obj An object of type \code{traces.obj}.
+#' @export
+
 #traces = pepTraces
 #annotation_table <- "/Users/ibludau/Downloads/uniprot-proteome%3AUP000000625.txt"
 #annotation_table <- read.table(annotation_table,sep = "\t",header = TRUE)
