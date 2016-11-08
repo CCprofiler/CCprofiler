fe38c9db69c856cd9c518826fbf087c2cb9564c0
mffrank <mfrank@student.ethz.ch>
2016-11-08 17:03:18 +0100

    Extended importFromOpenSwath function to make import of nonproteotypic peptides corresponding to a single canonical uniprot id possible

--- R/importFromOpenSWATH.R
+++ R/importFromOpenSWATH.R
@@ -9,6 +9,8 @@
 #'     `to a SEC elution fraction.
 #' @param remove_requantified Whether requantified (noise) peak group quantities
 #'     (as indicated by m_score = 2) should be removed, defaults to TRUE.
+#' @param also import nonproteotypic peptides corresponding to a single
+#'      canonical uniprot id(default FALSE)
 #' @return An object of class Traces containing 
 #'     "traces", "traces_type", "traces_annotation" and "fraction_annotation" list entries
 #'     that can be processed with the herein contained functions.
@@ -16,7 +18,8 @@
 importFromOpenSWATH <- function(data= 'OpenSwathData.tsv', 
                                 annotation.table="annotation.txt",
                                 remove_requantified=TRUE,
-                                MS1Quant=FALSE, rm.decoy = FALSE)
+                                MS1Quant=FALSE, rm.decoy = FALSE,
+                                collapseIsoforms = FALSE)
   {
   
   # read data & annotation table
@@ -30,27 +33,17 @@ importFromOpenSWATH <- function(data= 'OpenSwathData.tsv',
   }
   
   annotation <- data.table::fread(annotation.table)
+
   
-  #remove non-proteotypic discarding/keeping Decoys
-  message('removing non-unique proteins only keeping proteotypic peptides ...')
-  if (rm.decoy == TRUE){
-    message('remove decoys ...')
-    data <- data[grep("^1/", data$ProteinName)] 
-  } else {
-    data <- data[c(grep("^1/", data$ProteinName), grep("^DECOY_1/", data$ProteinName))] 
-  }
-  data$ProteinName <- gsub("1\\/","",data$ProteinName) 
-  
-  
-  # convert ProteinName to uniprot ids
-  if (length(grep("\\|",data$ProteinName)) > 0) {
-    message('converting ProteinName to uniprot ids ...')
-    decoy_idx <- grep("^DECOY_",data$ProteinName)
-    data$ProteinName <- gsub(".*\\|(.*?)\\|.*", "\\1", data$ProteinName)
-    data$ProteinName[decoy_idx] <- paste0("DECOY_",data$ProteinName[decoy_idx])
-    # the above does not wrk further downstream because "1/" is removed
-    #data$ProteinName <- extractIdsFromFastaHeader(data$ProteinName)
-  }
+  # # convert ProteinName to uniprot ids
+  # if (length(grep("\\|",data$ProteinName)) > 0) {
+  #   message('converting ProteinName to uniprot ids ...')
+  #   decoy_idx <- grep("^DECOY_",data$ProteinName)
+  #   data$ProteinName <- gsub(".*\\|(.*?)\\|.*", "\\1", data$ProteinName)
+  #   data$ProteinName[decoy_idx] <- paste0("DECOY_",data$ProteinName[decoy_idx])
+  #   # the above does not wrk further downstream because "1/" is removed
+  #   #data$ProteinName <- extractIdsFromFastaHeader(data$ProteinName)
+  # }
   
   # subset data to some important columns to save RAM
   if (MS1Quant == TRUE) {
@@ -70,6 +63,51 @@ importFromOpenSWATH <- function(data= 'OpenSwathData.tsv',
   
   rm(data)
   gc()
+  
+  # Remove Decoys if specified
+  
+  if (rm.decoy == TRUE){
+    message('removing decoys ...')
+    n <- nrow(data.s)
+    data.s <- data.s[!grep("^DECOY", data.s$ProteinName)]
+    message('removed ', n - nrow(data.s), ' decoys')
+  }
+  
+  # remove non-proteotypic peptides ,or peptides that match multiple canonical Protein Ids
+  # (ifcollapseIsoforms == T)
+  
+  if (collapseIsoforms) {
+    message("removing non-unique proteins only keeping peptides with single canonical Uniprot ID...")
+    
+    decoys_id <- grep("^DECOY", data.s$ProteinName)
+    data.s$ProteinName[decoys_id] <- gsub("^DECOY_", "", data.s$ProteinName[decoys_id])
+    isoform_no <- as.numeric(gsub("/.*", "", data.s$ProteinName))
+    a <- strsplit(data.s$ProteinName, split = "\\|")
+    uniprots <- lapply(1:length(a), function(i) a[[i]][seq(from = 2, to = isoform_no[i] * 2, by = 2)])
+    # when dash is removed all ids should be equal
+    canonical_typic <- sapply(uniprots, function(x) length(unique(gsub("-.*", "", x))) == 1)
+    #Paste the full peptide name with all Isoforms onto the FullPeptideName
+    data.s$FullPeptideName <- paste0(data.s$FullPeptideName, "-", data.s$ProteinName)
+    data.s$ProteinName <- paste0(gsub("-.*", "", sapply(a, `[`, 2)))
+    # data.s$ProteinName <- paste0(sapply(a,`[`,1),"|",
+    # gsub("-.*","",sapply(a,`[`,2)),"|",gsub("/sp","",sapply(a,`[`,3)))
+    data.s$ProteinName[decoys_id] <- paste0("DECOY_",data.s$ProteinName[decoys_id])
+    data.s <- data.s[canonical_typic]
+    message("Removed ", length(isoform_no) - nrow(data.s), " non unique peptides out of ",
+            length(isoform_no))
+    #data.s <- rbind(data.s, decoys)
+    
+  } else {
+    
+    #remove non-proteotypic discarding/keeping Decoys
+    
+    message('removing non-unique proteins only keeping proteotypic peptides ...')
+    data.s <- data.s[c(grep("^1/", data.s$ProteinName), grep("^DECOY_1/", data.s$ProteinName))] 
+    data.s$ProteinName <- gsub("1\\/","",data.s$ProteinName)
+    
+  }
+  
+  # Remove requantified peptides
   
   if (remove_requantified == TRUE) {
       data.s <- data.s[m_score < 2, ]
