d3a3f7b404c69d56d987137fbdf22d35067a46da
heuselm <heusel@imsb.biol.ethz.ch>
2017-02-20 13:51:08 +0100

    EXTEND annotateTraces to specifiy id column of traces obj, robust to annotating pepTraces with protein level info

--- R/annotateTraces.R
+++ R/annotateTraces.R
@@ -14,7 +14,12 @@
 #' @param replace_whitespace whether spaces contained in the anntation table column names shall be replaced by "_"
 #' defaults to TRUE
 #' ,typically FullPeptideName or Uniprot Entry names as protein_id. Same as in traces$traces$id
-annotateTraces <- function(traces, annotation_table = "uniprot.tab", id_column = "Entry", replace_whitespace = TRUE){
+annotateTraces <- function(traces,
+                           traces_id_column = "id",
+                           annotation_table = "uniprot.tab",
+                           annotation_table_id_column = "Entry",
+                           replace_whitespace = TRUE){
+  
   
   # use or if path to file read annotation table and add id column
   if (isTRUE(class(annotation_table) == "character")){
@@ -22,7 +27,12 @@ annotateTraces <- function(traces, annotation_table = "uniprot.tab", id_column =
   } else if (isTRUE(class(annotation_table) == "data.frame")){
     annotation_table <- as.data.table(annotation_table)
   }
-  annotation_table$id = annotation_table[[id_column]]
+  
+  # test if the annotation column ids match those in traces
+  if(sum(traces$trace_annotation[[traces_id_column]] %in% annotation_table[[annotation_table_id_column]]) < 1) {
+    stop(paste("The ids in the annotation_table column", annotation_table_id_column,
+    "do not match the IDs in the traces$trace_annotation$id column.")   ) 
+  }
   
   if (replace_whitespace) {
    names(annotation_table) <- gsub(" ", "_", names(annotation_table))
@@ -34,10 +44,8 @@ annotateTraces <- function(traces, annotation_table = "uniprot.tab", id_column =
     annotation_table[,protein_mw := protein_mw/1000]
   }
   
-  
-  
   new_annotation <- merge(traces$trace_annotation,
-                          annotation_table, by = "id", all.x=TRUE)
+                          annotation_table, by.x = traces_id_column, by.y = annotation_table_id_column , all.x=TRUE)
   traces$trace_annotation <- new_annotation
   
   # sort both trace_annotation and traces in case merge conducted an auto-sort
