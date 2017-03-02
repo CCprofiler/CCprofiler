f916c99f593ad5fa969bd38103d2d767f03f20d1
Isabell Bludau <ibludau@euler09.hpc-lca.ethz.ch>
2017-03-02 14:41:17 +0100

    remove deserted functions to clean up - renew namespace

--- R/convertSECToMW.R
+++
@@ -1,9 +0,0 @@
-#' Convert a fraction number to an estimated molecular weight.
-#'
-#' @param SECfraction The fraction number.
-#' @return Apparent MW at this SEC fraction in kDa.
-#' @export
-convertSECToMW <- function(SECfraction) {
-    logMW <- -0.1043329 * SECfraction + 9.682387
-    exp(logMW)
-}
