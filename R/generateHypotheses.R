32d9cd8ed4970f492d9b04408765ef483a9117e8
Isabell Bludau <ibludau@euler03.hpc-lca.ethz.ch>
2017-03-14 14:17:56 +0100

    generate complex hypotheses documentation & parameter settings

--- R/generateHypotheses.R
+++ R/generateHypotheses.R
@@ -1,4 +1,5 @@
-#' Generate binary network from concrete complex hypotheses (e.g. CORUM).
+#' generateBinaryNetwork
+#' @description Generate binary network from concrete complex hypotheses (e.g. CORUM).
 #' @import data.table
 #' @param complex_hypothesis data.table in the format of complex hypotheses
 #' @return data.table with binary interactions between "a" and "b"
@@ -8,7 +9,7 @@ generateBinaryNetwork <- function(complex_hypothesis){
   binary_interactions <- data.table(a=character(0),b=character(0))
   # get all binary interactions for each complex in the complex hypothesis
   for(i in c(1:length(complexes))){
-    binary_interactions <- get_interactions(complex=complexes[i],hypothesis=complex_hypothesis,binary_interactions=binary_interactions)
+    binary_interactions <- appendBinaryInteractions(complex=complexes[i],hypothesis=complex_hypothesis,binary_interactions=binary_interactions)
   }
   setkey(binary_interactions,NULL)
   unique(binary_interactions) # remove duplicates
@@ -16,14 +17,14 @@ generateBinaryNetwork <- function(complex_hypothesis){
   binary_interactions[]
 }
 
-#' Append binary interaction table with additional complexes
+#' appendBinaryInteractions
+#' @description Append binary interaction table with additional complexes
 #' @import data.table
 #' @param complex character string with complex_id
 #' @param hypothesis data.table in the format of complex hypotheses
 #' @param data.table with binary interactions between "a" and "b"
 #' @return data.table with binary interactions between "a" and "b" appended by current complex interactions
-#' @export
-get_interactions <- function(complex,hypothesis,binary_interactions){
+appendBinaryInteractions <- function(complex,hypothesis,binary_interactions){
   complex_proteins <- unique(hypothesis$protein_id[which(hypothesis$complex_id == complex)]) # get all proteins within complex
   binaries <- as.list(data.table(combn(complex_proteins,2))) # create all binary combinations of the proteins in complex
   binaries_sort <- lapply(binaries,FUN=sort) # sort alphanumerically to make it possible to remove duplicates at the end
@@ -33,8 +34,8 @@ get_interactions <- function(complex,hypothesis,binary_interactions){
   binary_interactions
 }
 
-
-#' Calculate shortest paths between all members of a binary interaction network.
+#' calculatePathlength
+#' @description Calculate shortest paths between all members of a binary interaction network.
 #' This function creates a table that contains all possible combinations of 2 from
 #' all input proteins (binomial coeffitient (length(proteins) over 2)).
 #' @import data.table
@@ -43,7 +44,6 @@ get_interactions <- function(complex,hypothesis,binary_interactions){
 #' @return data.table with three columns (x,y,dist) containing all possible combinations
 #' of proteins and their shortest distance as determined by the input binary interaction table
 #' @export
-
 calculatePathlength <- function(binaryInteractionTable){
   proteins <- unique(c(binaryInteractionTable$a,binaryInteractionTable$b)) # get all protein ids in interaction table
   g <- graph_from_data_frame(binaryInteractionTable, directed = FALSE) # create undirected graph using the igraph package
@@ -57,7 +57,8 @@ calculatePathlength <- function(binaryInteractionTable){
   combinations[]
 }
 
-#' Generate target hypotheses from distance table. This is done by choosing all
+#' generateComplexTargets
+#' @description Generate target hypotheses from distance table. This is done by choosing all
 #' max_distance (e.g. 1st) degree interactors for each protein in a distance table.
 #' @import data.table
 #' @param dist_info data.table with three columns (x,y,dist) containing all possible combinations
@@ -66,7 +67,6 @@ calculatePathlength <- function(binaryInteractionTable){
 #' @param max_distance numeric maximum distance between to proteins to be considered as being in one complex
 #' @return data.table in the format of complex hypotheses
 #' @export
-
 generateComplexTargets <- function(dist_info,max_distance=1){
   all_proteins <- unique(c(dist_info$x,dist_info$y))
   complex_table <- data.table(complex_id=character(0),complex_name=character(0),protein_id=character(0))
@@ -92,7 +92,8 @@ generateComplexTargets <- function(dist_info,max_distance=1){
   complex_table[]
 }
 
-#' Generate list of proteins for one complex decoy hypothesis
+#' generateDecoys
+#' @description Generate list of proteins for one complex decoy hypothesis
 #' @import data.table
 #' @param size numeric number of proteins to generate for complex decoy hypothesis
 #' @param all_proteins character vector of all possible protein_ids
@@ -101,7 +102,6 @@ generateComplexTargets <- function(dist_info,max_distance=1){
 #' This is the output from calculatePathlength()
 #' @param min_distance numeric minimum distance between to proteins to be able to be in same complex decoy
 #' @return data.table with binary interactions between "a" and "b"
-#' @export
 generateDecoys <- function(size,all_proteins,dist_info,min_distance){
   complex_proteins <- vector(mode="character",length=size)
   for(j in c(1:size)){
@@ -117,7 +117,8 @@ generateDecoys <- function(size,all_proteins,dist_info,min_distance){
   paste(complex_proteins,collapse=";")
 }
 
-#' Generate complex decoy hypotheses from target hypotheses and distance table.
+#' generateComplexDecoys
+#' @description Generate complex decoy hypotheses from target hypotheses and distance table.
 #' Selects proteins by min_distance in the network.
 #' @import data.table
 #' @param target_hypothesis data.table in the format of complex hypotheses
@@ -125,34 +126,55 @@ generateDecoys <- function(size,all_proteins,dist_info,min_distance){
 #' of proteins and their shortest distance.
 #' This is the output from calculatePathlength()
 #' @param min_distance numeric minimum distance between to proteins to be able to be in same complex decoy
+#' @param append logical if to append decoys to the input target hypotheses, default is FALSE
 #' @param seed numeric seed for random number generator
+#' @param parallelized logical, if the computation should be done in parallel, default=FALSE
 #' @param n.cores numeric number of cores used for parallelization
 #' @return data.table with binary interactions between "a" and "b"
 #' @export
-generateComplexDecoys <- function(target_hypothesis,dist_info,min_distance=2,seed=123,n.cores=1){
+generateComplexDecoys <- function(target_hypothesis,dist_info,min_distance=2,append=FALSE,seed=123,parallelized=FALSE,n.cores=1){
+  input = target_hypothesis
   proteins <- unique(target_hypothesis$protein_id)
   target_hypothesis <- target_hypothesis[,list(protein_count=length(complex_id)),by=list(unique.complex_id=complex_id)]
   size=target_hypothesis$protein_count
-  cl <- snow::makeCluster(n.cores)
-  # setting a seed is absolutely crutial to ensure reproducible results!!!!!!!!!!!!!!!!!!!
-  clusterSetRNGStream(cl,seed)
-  doSNOW::registerDoSNOW(cl)
-  clusterExport(cl, c("generateDecoys"))
-  pb <- txtProgressBar(max = length(size), style = 3)
-  progress <- function(n) setTxtProgressBar(pb, n)
-  opts <- list(progress = progress)
-  set.seed(seed)
-  #decoy_list <- parLapply(cl=cl,X=size,fun=generateDecoys,all_proteins=proteins,dist_info=dist_info,min_distance=min_distance)
-  decoy_list <- foreach(i=seq_along(size),.options.snow = opts) %dopar% {
-    x = size[i]
-    generateDecoys(size=x,all_proteins=proteins,dist_info=dist_info,min_distance=min_distance)
-    }
-  close(pb)
-  parallel::stopCluster(cl)
+  if (parallelized) {
+    cl <- snow::makeCluster(n.cores)
+    # setting a seed is absolutely crutial to ensure reproducible results!!!!!!!!!!!!!!!!!!!
+    clusterSetRNGStream(cl,seed)
+    doSNOW::registerDoSNOW(cl)
+    clusterExport(cl, c("generateDecoys"))
+    pb <- txtProgressBar(max = length(size), style = 3)
+    progress <- function(n) setTxtProgressBar(pb, n)
+    opts <- list(progress = progress)
+    set.seed(seed)
+    #decoy_list <- parLapply(cl=cl,X=size,fun=generateDecoys,all_proteins=proteins,dist_info=dist_info,min_distance=min_distance)
+    decoy_list <- foreach(i=seq_along(size),.options.snow = opts) %dopar% {
+      x = size[i]
+      generateDecoys(size=x,all_proteins=proteins,dist_info=dist_info,min_distance=min_distance)
+      }
+    close(pb)
+    parallel::stopCluster(cl)
+  } else {
+    set.seed(seed)
+    pb <- txtProgressBar(max = length(size), style = 3)
+    decoy_list <- foreach(i=seq_along(size)) %do% {
+      setTxtProgressBar(pb, i)
+      x = size[i]
+      generateDecoys(size=x,all_proteins=proteins,dist_info=dist_info,min_distance=min_distance)
+      }
+    close(pb)
+  }
   #decoy_list <- lapply(size,generateDecoys,all_proteins=proteins,dist_info=dist_info,min_distance=min_distance)
   decoy_list <- unlist(decoy_list)
-  decoy_hypothesis <- data.table(complex_id=paste0("decoy_",seq(1,length(decoy_list),1)),complex_name=paste0("decoy complex ",seq(1,length(decoy_list),1)),protein_id=decoy_list)
+  decoy_hypothesis <- data.table(complex_id=paste0("d",seq(1,length(decoy_list),1)),complex_name=paste0("decoy_complex_",seq(1,length(decoy_list),1)),protein_id=decoy_list)
   decoy_hypothesis <- decoy_hypothesis[,list(protein = unlist(strsplit(protein_id, ";"))), by=c("complex_id","complex_name")]
   names(decoy_hypothesis)[3] <- "protein_id"
-  decoy_hypothesis[]
+  if (append == TRUE) {
+    appended_hypotheses <- rbind(input,decoy_hypothesis)
+    appended_hypotheses[]
+  } else if (append == FALSE) {
+    decoy_hypothesis[]
+  }  else {
+    stop("Invalid parameter for append. Please select TRUE or FALSE.")
+  }
 }
