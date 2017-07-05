81d1cc5de51242255663b8212a8d85a43c056f3d
Isabell Bludau <ibludau@euler07.hpc-lca.ethz.ch>
2017-07-05 14:48:31 +0200

    add test for annotateTraces

---
+++ tests/testthat/test_annotateTraces.R
@@ -0,0 +1,19 @@
+context("annotateTraces")
+
+annotatedTraces <- annotateTraces(traces=examplePeptideTracesUnannotated,
+                                  annotation_table=exampleTraceAnnotation,
+                                  traces_id_column = "protein_id",
+                                  annotation_table_id_column = "Entry",
+                                  annotation_table_mass_column = "Mass",
+                                  uniprot_mass_format = TRUE,
+                                  replace_whitespace = TRUE)
+
+test_that("Output format",{
+  # test if untouched traces parts stay identical
+  testthat::expect_identical(examplePeptideTracesUnannotated$traces, annotatedTraces$traces)
+  testthat::expect_identical(examplePeptideTracesUnannotated$trace_type, annotatedTraces$trace_type)
+  testthat::expect_identical(examplePeptideTracesUnannotated$fraction_annotation, annotatedTraces$fraction_annotation)
+  testthat::expect_identical(examplePeptideTracesUnannotated$trace_annotation$id, annotatedTraces$trace_annotation$id)
+  # test if annotation was successful
+  testthat::expect_true(all(names(exampleTraceAnnotation) %in% c(names(annotatedTraces$trace_annotation),"Entry")))
+  })
