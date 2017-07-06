d637883600cd54d10e48a6ee06420e440311264c
Isabell Bludau <ibludau@euler05.hpc-lca.ethz.ch>
2017-07-06 18:31:08 +0200

    add documentation and change examplePCPdataWide

--- tests/testthat/test_annotateTraces.R
+++ tests/testthat/test_annotateTraces.R
@@ -15,7 +15,8 @@ test_that("Output format",{
   testthat::expect_identical(examplePeptideTracesUnannotated$traces, annotatedTraces$traces)
   testthat::expect_identical(examplePeptideTracesUnannotated$trace_type, annotatedTraces$trace_type)
   testthat::expect_identical(examplePeptideTracesUnannotated$fraction_annotation, annotatedTraces$fraction_annotation)
-  testthat::expect_identical(examplePeptideTracesUnannotated$trace_annotation$id, annotatedTraces$trace_annotation$id)
+  testthat::expect_true(all(examplePeptideTracesUnannotated$trace_annotation$id %in% annotatedTraces$trace_annotation$id))
+  testthat::expect_true(all(annotatedTraces$trace_annotation$id %in% examplePeptideTracesUnannotated$trace_annotation$id))
   ## test if annotation was successful
   testthat::expect_true(all(names(exampleTraceAnnotation) %in% c(names(annotatedTraces$trace_annotation),"Entry")))
   })
