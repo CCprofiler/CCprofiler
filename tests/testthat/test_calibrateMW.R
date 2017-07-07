62ffed562edd36ad1ff3ea87832fc947aca283ff
ibludau <bludau@imsb.biol.ethz.ch>
2017-07-07 14:20:08 +0200

    Update test_calibrateMW.R
    Clean-up confusions

--- tests/testthat/test_calibrateMW.R
+++ tests/testthat/test_calibrateMW.R
@@ -1,19 +1,19 @@
 context("calibrateMW")
 
-calibrationFunctions <- calibrateSECMW(exampleCalibrationTable,
-                                       plot = T,
+calibrationFunctions <- calibrateMW(exampleCalibrationTable,
+                                       plot = FALSE,
                                        PDF = FALSE)
 
 test_that("Fringe cases", {
-  testthat::expect_error(calibrateSECMW(c()),"calibration_table input is neither file name or data.table")
-  testthat::expect_error(calibrateSECMW(data.table()),"calibration_table seems to be empty")
+  testthat::expect_error(calibrateMW(c()),"calibration_table input is neither file name or data.table")
+  testthat::expect_error(calibrateMW(data.table()),"calibration_table seems to be empty")
   
 })
 test_that("Output Format", {
-  testthat::expect_equal(calibrationFunctions$MWtoSECfraction(0), Inf)
-  testthat::expect_lt(min(exampleCalibrationTable$std_weights_kDa),Inf)
-  testthat::expect_gt(min(exampleCalibrationTable$std_weights_kDa),0)
-  testthat::expect_lt(max(exampleCalibrationTable$std_weights_kDa),Inf)
-  testthat::expect_gt(max(exampleCalibrationTable$std_weights_kDa),0)
+  testthat::expect_equal(calibrationFunctions$MWtoFraction(0), Inf)
+  testthat::expect_lt(calibrationFunctions$MWtoFraction(min(exampleCalibrationTable$std_weights_kDa)),Inf)
+  testthat::expect_gt(calibrationFunctions$MWtoFraction(min(exampleCalibrationTable$std_weights_kDa)),0)
+  testthat::expect_lt(calibrationFunctions$FractionToMW(max(exampleCalibrationTable$std_elu_fractions)),Inf)
+  testthat::expect_gt(calibrationFunctions$FractionToMW(min(exampleCalibrationTable$std_elu_fractions)),0)
   
 })
