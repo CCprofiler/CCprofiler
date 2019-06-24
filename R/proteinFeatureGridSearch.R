d51052eb34b135b6181538be5527a7917c2579e7
Isabell Bludau <ibludau@eu-login-01-ng.euler.ethz.ch>
2019-06-24 14:43:03 +0200

    fix parameter grid search call

--- R/proteinFeatureGridSearch.R
+++ R/proteinFeatureGridSearch.R
@@ -2,7 +2,7 @@
 #' Perform Protein feature grid search
 #' @description Perform Protein feature finding (calls \code{\link{findProteinFeatures}})
 #' with all possible combinations of the specified parameters (grid search).
-#' @details The runtime of this function scales with the binomial coefficient of the total 
+#' @details The runtime of this function scales with the binomial coefficient of the total
 #' number of parameters specified, and can therefore take a long time. If many parameter combinations
 #' are searched the use of multiple cores for parallel computation is strongly recommended.
 #' @import data.table
@@ -15,14 +15,14 @@
 #' @return List of search result tables for every possible parameter combination.
 #' The result tables contain additional columns specifying the parameters.
 #' @export
-#' @examples  
-#' 
+#' @examples
+#'
 #' ## Load example data and subset to reduce runtime
 #' peptideTraces <- examplePeptideTraces
 #' peptideTraces <- subset(peptideTraces,
 #'                         trace_subset_ids = unique(peptideTraces$trace_annotation$protein_id)[2:3],
 #'                         trace_subset_type = "protein_id")
-#' 
+#'
 #' ## Perform a small grid search for 2 parameter combinations
 #' # Depending on the computational resources this can take several minutes
 #' gridList <- performProteinGridSearch(traces = peptideTraces,
@@ -31,7 +31,7 @@
 #'                                      smoothing = 9,
 #'                                      rt_heights = 5,
 #'                                      n_cores = 2)
-#'  
+#'
 #' lapply(gridList, head, n = 2)
 
 
@@ -41,13 +41,13 @@ performProteinGridSearch <- function(traces,
                                      smoothing = c(7,9,11),
                                      rt_heights = c(3,4,5),
                                      n_cores=1){
-  
+
   .tracesTest(traces, type = "peptide")
   .testGridParameter(corrs, "corrs")
   .testGridParameter(windows, "windows")
   .testGridParameter(smoothing, "smoothing")
   .testGridParameter(rt_heights, "rt_heights")
-  
+
   parameter_grid <- expand.grid(corrs,windows,smoothing,rt_heights)
   names(parameter_grid) <- c("corr","window","smoothing","rt_height")
   if(n_cores > nrow(parameter_grid)){
@@ -58,9 +58,9 @@ performProteinGridSearch <- function(traces,
   # setting a seed is absolutely crutial to ensure reproducible results!
   clusterSetRNGStream(cl,123)
   doSNOW::registerDoSNOW(cl)
-  clusterEvalQ(cl,library(SECprofiler,data.table))
+  clusterEvalQ(cl,library(CCprofiler,data.table))
   # clusterExport(cl, list("generateRandomPepTraces"))
-  
+
   data <- parRapply(cl,parameter_grid,
                     FUN = .runGridProteinFeatureFinding,
                     pepTraces = traces)
@@ -77,7 +77,7 @@ performProteinGridSearch <- function(traces,
                             rt_height=as.numeric(params["rt_height"]),
                             smoothing_length=as.numeric(params["smoothing"]),
                             useRandomDecoyModel=TRUE)
-  
+
   res[,corr:=as.numeric(params["corr"])]
   res[,window:=as.numeric(params["window"])]
   res[,rt_height:=as.numeric(params["rt_height"])]
