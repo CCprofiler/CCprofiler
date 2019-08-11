742f8b4b5e38d35747db8366c07360d2b06fe119
ibludau <bludau@imsb.biol.ethz.ch>
2019-08-11 12:30:40 +0200

    fix include suffix names in complex hypothesis summaries

--- R/plotSummarizedMScoverage.R
+++ R/plotSummarizedMScoverage.R
@@ -13,13 +13,13 @@
 #' @param PDF Logical whether to save plots in PDF, default = \code{FALSE}.
 #' @return Multiple plots summarizing the MS coverage.
 #' @export
-plotSummarizedMScoverage <- function(hypotheses,protTraces,PDF=FALSE){
+plotSummarizedMScoverage <- function(hypotheses,protTraces,PDF=FALSE, name_suffix="complexHypotheses"){
   .tracesTest(protTraces, type="protein")
   hypotheses <- hypotheses[grep("DECOY",hypotheses$complex_id,invert=TRUE)]
   proteins_in_hypotheses <- unique(hypotheses$protein_id)
   proteins_in_traces <- unique(protTraces$traces$id)
   proteinMScoverageSummary <- data.table(name=c("not detected","detected"),count=c(sum(!(proteins_in_hypotheses %in% proteins_in_traces)),sum(proteins_in_hypotheses %in% proteins_in_traces)))
-  plotProteinMScoveragePie(proteinMScoverageSummary,PDF=PDF)
+  plotProteinMScoveragePie(proteinMScoverageSummary,PDF=PDF,name_suffix=name_suffix)
   hypotheses[,annotated:=1]
   hypotheses[,detected:=ifelse(protein_id %in% proteins_in_traces, 1, 0)]
   #hypotheses[,protein_collapsed := paste(protein_id,collapse=";"),by=complex_id]
@@ -28,13 +28,13 @@ plotSummarizedMScoverage <- function(hypotheses,protTraces,PDF=FALSE){
   hypotheses[,ms_completeness := detected_collapsed/annotated_collapsed,by=complex_id]
   unique_hypotheses <- unique(hypotheses,by="complex_id")
   complexMScoverageSummary <- data.table(name=c("not detected","< 50% detected",">= 50% detected","fully detected"),
-                                        count=c(sum(unique_hypotheses$ms_completeness == 0),
-                                        sum((unique_hypotheses$ms_completeness > 0) & (unique_hypotheses$ms_completeness < 0.5)),
-                                        sum((unique_hypotheses$ms_completeness < 1) & (unique_hypotheses$ms_completeness >= 0.5)),
-                                        sum(unique_hypotheses$ms_completeness == 1))
-                                        )
-  plotComplexMScoveragePie(complexMScoverageSummary,PDF=PDF)
-  plotComplexMScoverageScatter(unique_hypotheses,PDF=PDF)
+                                         count=c(sum(unique_hypotheses$ms_completeness == 0),
+                                                 sum((unique_hypotheses$ms_completeness > 0) & (unique_hypotheses$ms_completeness < 0.5)),
+                                                 sum((unique_hypotheses$ms_completeness < 1) & (unique_hypotheses$ms_completeness >= 0.5)),
+                                                 sum(unique_hypotheses$ms_completeness == 1))
+  )
+  plotComplexMScoveragePie(complexMScoverageSummary,PDF=PDF,name_suffix=name_suffix)
+  plotComplexMScoverageScatter(unique_hypotheses,PDF=PDF,name_suffix=name_suffix)
 }
 
 #' plotProteinMScoveragePie.
@@ -44,15 +44,15 @@ plotSummarizedMScoverage <- function(hypotheses,protTraces,PDF=FALSE){
 #' @param proteinMScoverageSummary data.table with summary
 #' @param PDF logical default = TRUE
 #' @return plots summarizing the MS coverage
-plotProteinMScoveragePie <- function(proteinMScoverageSummary,PDF=FALSE){
+plotProteinMScoveragePie <- function(proteinMScoverageSummary,PDF=FALSE, name_suffix="complexHypotheses"){
   cbPalette <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
   if(PDF){
-    pdf("proteinMScoveragePie.pdf")
+    pdf(paste0("proteinMScoveragePie_",name_suffix,".pdf"))
   }
   print(pie(x=proteinMScoverageSummary$count,
             labels=paste0(proteinMScoverageSummary$name,"\n",proteinMScoverageSummary$count),
             col=cbPalette[1:nrow(proteinMScoverageSummary)],
-            main = "Protein coverage"))
+            main = paste0("Protein coverage ",name_suffix)))
   if(PDF){
     dev.off()
   }
@@ -65,15 +65,15 @@ plotProteinMScoveragePie <- function(proteinMScoverageSummary,PDF=FALSE){
 #' @param complexMScoverageSummary data.table with summary
 #' @param PDF logical default = TRUE
 #' @return plots summarizing the MS coverage
-plotComplexMScoveragePie <- function(complexMScoverageSummary,PDF=TRUE){
+plotComplexMScoveragePie <- function(complexMScoverageSummary,PDF=TRUE, name_suffix="complexHypotheses"){
   cbPalette <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
   if(PDF){
-    pdf("complexMScoveragePie.pdf")
+    pdf(paste0("complexMScoveragePie_",name_suffix,".pdf"))
   }
   print(pie(x=complexMScoverageSummary$count,
             labels=paste0(complexMScoverageSummary$name,"\n",complexMScoverageSummary$count),
             col=cbPalette[1:nrow(complexMScoverageSummary)],
-            main = "Hypothesis coverage"))
+            main = paste0("Hypothesis coverage ", name_suffix)))
   if(PDF){
     dev.off()
   }
@@ -86,9 +86,9 @@ plotComplexMScoveragePie <- function(complexMScoverageSummary,PDF=TRUE){
 #' @param complexMScoverage data.table with summary
 #' @param PDF logical default = TRUE
 #' @return plots summarizing the MS coverage
-plotComplexMScoverageScatter <- function(complexMScoverage,PDF=FALSE){
+plotComplexMScoverageScatter <- function(complexMScoverage,PDF=FALSE, name_suffix="complexHypotheses"){
   if(PDF){
-    pdf("complexMScoverageScatter.pdf")
+    pdf(paste0("complexMScoverageScatter_",name_suffix,".pdf"))
   }
   p <- ggplot(data=complexMScoverage,aes(x=annotated_collapsed,y=detected_collapsed,colour=ms_completeness)) +
     geom_point() +
@@ -103,7 +103,7 @@ plotComplexMScoverageScatter <- function(complexMScoverage,PDF=FALSE){
           panel.grid.minor = element_blank()) +
     labs(colour = "MS completeness\n") +
     theme(legend.justification=c(0,1), legend.position=c(0.05,0.75)) +
-    ggtitle("Hypothesis coverage") + 
+    ggtitle(paste0("Hypothesis coverage ",name_suffix)) + 
     theme(plot.title = element_text(size=14, face="bold"))
   print(p)
   if(PDF){
