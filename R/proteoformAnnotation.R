d433ad78e34b968172f567241f352b4ac57e71fa
ibludau <bludau@imsb.biol.ethz.ch>
2020-03-18 16:16:03 +0100

    FEATURE combineTracesMutiCond

--- R/proteoformAnnotation.R
+++ R/proteoformAnnotation.R
@@ -76,3 +76,62 @@ filterByMaxCorr <- function(traces, cutoff = 0.85,
   traces_filt <- subset(traces, filterpeps, "id")
   return(traces_filt)
 }
+
+#' Combine tracesList across conditions to one traces object with all
+#' the traces of all conditions added as additional fractions for
+#' peptide clustering across conditions.
+#' @param tracesList Object of class tracesList.
+#' @return Object of class traces
+#' @export
+combineTracesMutiCond <- function(tracesList){
+  .tracesListTest(tracesList)
+  cond <- names(tracesList)
+  idx <- seq_along(cond)
+  traces <- lapply(idx, function(i){
+    t <- tracesList[[i]]
+    trac <- t$traces
+    trac.m <- melt(trac,id.vars="id")
+    trac.m[, cond := cond[[i]]]
+    return(trac.m)
+    })
+  traces_combi <- rbindlist(traces)
+  traces_all <- dcast(traces_combi,id ~ cond+variable,value.var="value",fill=0)
+  names(traces_all) <- c("id",seq(1,ncol(traces_all)-1,1))
+  setcolorder(traces_all, c(seq(1,ncol(traces_all)-1,1),"id"))
+
+  trace_type_all <- tracesList[[1]]$trace_type
+
+  trace_annotation <- lapply(tracesList, function(t){
+    # Use the first element as a template annotation
+    cols <- names(tracesList[[1]]$trace_annotation)
+    cols <- cols[which(!cols %in% c("SibPepCorr","RepPepCorr","n_peptides"))]
+    res <- subset(t$trace_annotation,select=cols)
+    return(res)
+    })
+  trace_annotation_combi <- rbindlist(trace_annotation)
+  trace_annotation_combi <- unique(trace_annotation_combi)
+  trace_annotation_combi <- trace_annotation_combi[order(match(id,
+                                                          traces_all$id))]
+
+  fraction_annotation_all <- data.table(id=seq(1,ncol(traces_all)-1,1))
+
+  combi_traces <- list(
+    traces = traces_all,
+    trace_type = trace_type_all,
+    trace_annotation = trace_annotation_combi,
+    fraction_annotation = fraction_annotation_all
+  )
+
+  if(!is.null(tracesList[[1]][["genomic_coord"]])){
+	coo <- tracesList[[1]][["genomic_coord"]]
+	for (i in c(2:length(tracesList))) {
+		coo <- append(coo, tracesList[[i]][["genomic_coord"]], after = length(coo))
+	}
+	coo <- coo[!duplicated(names(coo))]
+	combi_traces$genomic_coord <- coo
+  }
+
+  class(combi_traces) <- "traces"
+  .tracesTest(combi_traces)
+  return(combi_traces)
+}
