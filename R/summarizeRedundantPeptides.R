584900e648fb13b543e7f6dc0c28d2b4df1c122e
Isabell Bludau <isabell@Isabells-MBP.fritz.box>
2020-04-13 18:36:33 +0200

    FIX summarize all redundant also parially overlapping peptides

--- R/summarizeRedundantPeptides.R
+++ R/summarizeRedundantPeptides.R
@@ -7,7 +7,9 @@
 #' @return An object of type traces, trace_type is peptide.
 #' @export
 summarizeAlternativePeptideSequences <- function(traces,
-                                  topN = 1,position="PeptidePositionStart",
+                                  topN = 1,
+                                  start="PeptidePositionStart",
+                                  end="PeptidePositionEnd",
                                   verbose = TRUE){
   UseMethod("summarizeAlternativePeptideSequences", traces)
 }
@@ -17,60 +19,87 @@ summarizeAlternativePeptideSequences <- function(traces,
 #' genomic star position. These are e.g. peptides with missed cleavages.
 #' @export
 summarizeAlternativePeptideSequences.traces <- function(traces,topN=1,
-  position="PeptidePositionStart",
-  verbose = TRUE){
+                                                        start="PeptidePositionStart",
+                                                        end="PeptidePositionEnd",
+                                                        verbose = TRUE){
   .tracesTest(traces, "peptide")
-  if (! position %in% names(traces$trace_annotation)) {
+  if (! start %in% names(traces$trace_annotation)) {
+    stop("This function is only available for traces annotated by the
+    relative protein sequence position (annotateRelativePepPos).")
+  }
+  if (! end %in% names(traces$trace_annotation)) {
     stop("This function is only available for traces annotated by the
     relative protein sequence position (annotateRelativePepPos).")
   }
   annotation_old <- copy(traces$trace_annotation)
   traces_old <- copy(traces)
   traces_old$trace_annotation[,protein_id_original := protein_id]
-  traces_old$trace_annotation[,protein_id := paste0(protein_id,"_",get(position))]
-  traces_summed <- proteinQuantification.traces(traces_old,
-                                  topN = topN,
-                                  keep_less = TRUE,
-                                  rm_decoys = TRUE,
-                                  use_sibPepCorr = FALSE,
-                                  use_repPepCorr = FALSE,
-                                  full_intersect_only = FALSE,
-                                  quantLevel = "protein_id",
-                                  verbose = verbose)
+  
+  traces_old$trace_annotation[, new_id := "new"]
+  
+  proteins <- unique(traces_old$trace_annotation$protein_id)
+  for(p in proteins){
+    #print(p)
+    p_ann <- subset(traces_old$trace_annotation, protein_id==p)
+    peptides <- unique(p_ann$id)
+    pep_seq <- list()
+    for (pep in peptides) {
+      pep_ann <- subset(p_ann, id==pep)
+      coPeps <- subset(p_ann, ((p_ann[[start]] >= pep_ann[[start]]) & (p_ann[[start]] <= pep_ann[[end]])) | 
+                         (p_ann[[end]] >= pep_ann[[start]]) & (p_ann[[end]] <= pep_ann[[end]])
+      )$id
+      pep_seq[[pep]] <- coPeps
+    }
+    pep_seq <- unique(pep_seq)
+    #all_peps <- list()
+    for (pep in peptides) {
+      #all_peps[[pep]] <- paste0(as.character(sort(unique(unlist(lapply(pep_seq, function(x){if(pep %in% x)x}))))), collapse=";")
+      traces_old$trace_annotation[(protein_id==p) & (id==pep)]$new_id <- paste0(as.character(sort(unique(unlist(lapply(pep_seq, function(x){if(pep %in% x)x}))))), collapse=";")
+    }
+  }
+  
+  traces_old$trace_annotation[,protein_id := paste0(protein_id,"_",new_id)]
+  
+  traces_summed <- proteinQuantification(traces_old,   #.traces
+                                         topN = topN,
+                                         keep_less = TRUE,
+                                         rm_decoys = TRUE,
+                                         use_sibPepCorr = FALSE,
+                                         use_repPepCorr = FALSE,
+                                         full_intersect_only = FALSE,
+                                         quantLevel = "protein_id",
+                                         verbose = verbose)
   traces_summed_new <- copy(traces_summed)
   ann <- subset(traces_summed_new$trace_annotation,select=c("protein_id","quant_peptides_used"))
   traces_summed_new$traces <- merge(traces_summed_new$traces,ann,by.x="id",by.y="protein_id")
   traces_summed_new$traces[,id:=NULL]
   setnames(traces_summed_new$traces,"quant_peptides_used","id")
   setorder(traces_summed_new$traces, -id)
-
+  
   traces_summed_new$trace_annotation <- subset(annotation_old,id %in% traces_summed_new$traces$id)
-  #traces_summed_new$trace_annotation <- copy(traces$trace_annotation)
-  #traces_summed_new$trace_annotation[,id:=quant_peptides_used]
-  #traces_summed_new$trace_annotation[,quant_peptides_used:=NULL]
-  #traces_summed_new$trace_annotation[,protein_id:=protein_id_original]
-  #traces_summed_new$trace_annotation[,protein_id_original:=NULL]
-  ##setnames(traces_summed_new$trace_annotation,"n_peptides","n_cleaved_peptides")
-  #traces_summed_new$trace_annotation[,n_peptides:=NULL]
+  
   setorder(traces_summed_new$trace_annotation, -id)
-
+  
   traces_summed_new$trace_type="peptide"
-
+  
   .tracesTest(traces_summed_new, "peptide")
-
+  
   return(traces_summed_new)
 }
 
 #' @describeIn summarizeAlternativePeptideSequences Select most abndant
 #' peptide for each group of peptides with same
 #' @export
-summarizeAlternativePeptideSequences.tracesList <- function(tracesList,topN=1,
-  position="PeptidePositionStart",
-  verbose = TRUE){
+summarizeAlternativePeptideSequences.tracesList <- function(traces,topN=1,
+                                                        start="PeptidePositionStart",
+                                                        end="PeptidePositionEnd",
+                                                        verbose = TRUE){
   .tracesListTest(tracesList, type = "peptide")
   res_list <- lapply(tracesList, summarizeAlternativePeptideSequences.traces,
-                topN=topN,position=position,
-                verbose = verbose)
+                     topN=topN,
+                     start=start,
+                     end=end,
+                     verbose = verbose)
   class(res) <- "tracesList"
   .tracesListTest(res)
   return(res)
